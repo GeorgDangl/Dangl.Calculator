8ed2dbcc5323c6ca8a0d65b94efda31a64f3f45e
Georg Dangl <dangl@iabi.eu>
2016-12-05 11:53:26 +0100

    Update ANTLR dependency

--- src/Dangl.Calculator/BuildANTLR.bat
+++ src/Dangl.Calculator/BuildANTLR.bat
@@ -2,6 +2,6 @@
 SET JAVACOMMAND=java
 where %JAVACOMMAND%
 if %ERRORLEVEL% NEQ 0 (
-	SET JAVACOMMAND=%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.3.1-beta001/tools/ikvm.exe
+	SET JAVACOMMAND=%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.4-beta001/tools/ikvm.exe
 )
-%JAVACOMMAND% -jar %UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.3.1-beta001/tools/antlr4-csharp-4.5.3.1-SNAPSHOT-complete.jar Calculator.g4 -package Dangl.Calculator.Generated -o Generated -Dlanguage=CSharp_v4_5 -visitor -no-listener
+%JAVACOMMAND% -jar %UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.4-beta001/tools/antlr4-csharp-4.5.4-SNAPSHOT-complete.jar Calculator.g4 -package Dangl.Calculator.Generated -o Generated -Dlanguage=CSharp_v4_5 -visitor -no-listener
