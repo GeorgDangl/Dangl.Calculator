7e4305fc02f6d23b9162c1331322feb310c9abfc
Georg Dangl <georg@dangl.me>
2017-08-11 19:38:43 +0200

    Update ANTLR4 to latest version and use ANTLR4 MSBuild task

--- src/Dangl.Calculator/BuildANTLR.bat
+++ src/Dangl.Calculator/BuildANTLR.bat
@@ -4,4 +4,5 @@ where %JAVACOMMAND%
 if %ERRORLEVEL% NEQ 0 (
 	SET JAVACOMMAND=%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.4-beta001/tools/ikvm.exe
 )
-%JAVACOMMAND% -jar %UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.4-beta001/tools/antlr4-csharp-4.5.4-SNAPSHOT-complete.jar Calculator.g4 -package Dangl.Calculator.Generated -o Generated -Dlanguage=CSharp_v4_5 -visitor -no-listener
+%JAVACOMMAND% -jar %UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.5.4-beta001/tools/antlr4-csharp-4.5.4-SNAPSHOT-complete.jar Calculator.g4
+-package Dangl.Calculator.Generated -o Generated -Dlanguage=CSharp_v4_5 -visitor -no-listener
